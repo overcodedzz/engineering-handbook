6cf656461f9cdef29b8f4a96be3372b4dbd4631c
minhdqdev <minhdq.dev@gmail.com>
2025-01-23 00:13:20 +0700

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,38 @@ import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 
 export const metadata = {
-  lang: "en",
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+  // description: 'Make beautiful websites with Next.js & MDX.',
+  // metadataBase: new URL('https://nextra.site'),
+  keywords: [
+    'OverCoded',
+    'Engineering',
+    'Handbook',
+  ],
+  applicationName: 'OverCoded Engineering Handbook',
+  appleWebApp: {
+    title: 'OverCoded Engineering Handbook'
+  },
+  title: {
+    default: 'OverCoded Engineering Handbook',
+    template: '%s | OverCoded Engineering Handbook'
+  },
+  // openGraph: {
+  //   // https://github.com/vercel/next.js/discussions/50189#discussioncomment-10826632
+  //   url: './',
+  //   siteName: 'Nextra',
+  //   locale: 'en_US',
+  //   type: 'website'
+  // },
+  other: {
+    'msapplication-TileColor': '#fff'
+  },
+  // twitter: {
+  //   site: 'https://nextra.site'
+  // },
+  // alternates: {
+  //   // https://github.com/vercel/next.js/discussions/50189#discussioncomment-10826632
+  //   canonical: './'
+  // }
 }
 
 const banner = <Banner storageKey="1.0-release">Version 1.0 is released 🎉</Banner>
